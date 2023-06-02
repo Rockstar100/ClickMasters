6d59ae651868eec043c6ae792ceef3fc9ff01d46
Rockstar100 <boyrockstar100@gmail.com>
2023-06-03 00:24:37 +0530

    changes

--- src/pages/Rider.js
+++ src/pages/Rider.js
@@ -1,129 +1,80 @@
-import React from 'react'
-import tw from 'tailwind-styled-components'
-import cardetail from "./cardetail"
-import { useEffect,useState } from 'react'
-import { Typography } from '@mui/material'
-import { on } from 'events'
+import React from 'react';
+import tw from 'tailwind-styled-components';
+import carDetail from "./cardetail";
+import { useEffect, useState } from 'react';
+import { Typography } from '@mui/material';
+import { on } from 'events';
 import { useRef } from 'react';
-
 import { useRouter } from 'next/router';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-const Rider = (props ) => {
-    const router = useRouter();
-    const [car,setCar] = useState([]);
-    
-    
-    useEffect(() => {
-        setCar(cardetail)
-    },[car])
-    const [selectedCar,setSelectedCar] = useState(null);
-    
-    const handleCarSelection = (id)=> {
+const Rider = (props) => {
+  const router = useRouter();
+  const [car, setCar] = useState([]);
 
-        setSelectedCar(id)
-        router.push({
-            pathname: '/Usercard',
-            query: {
-                pick: props.pickup,
-                sdate: props.sdate,
-                edate: props.edate,
-                detail : id
-            }})
-        
+  useEffect(() => {
+    setCar(carDetail);
+  }, []);
 
-    }
-    
-    
+  const [selectedCar, setSelectedCar] = useState(null);
 
+  const handleCarSelection = (id) => {
+    setSelectedCar(id);
+    router.push({
+      pathname: '/Usercard',
+      query: {
+        pick: props.pickup,
+        sdate: props.sdate,
+        edate: props.edate,
+        detail: id,
+      },
+    });
+  };
 
-
-
-    
-   
-    return (
-        <Wrapper>
-            <Title>select your Event</Title>
-            <CarList>
-                {/* <Link 
-                 href={{
-                    pathname: "/Usercard",
-                    query: {
-                        pick: props.pick,
-                        sdate: props.sdate,
-                        edate: props.edate,
-                        detail : cardetail.title
-                       
-    
-                    }}}> */}
-                
-            {cardetail.map((cardetail)=>
-            <Car key={cardetail.id
-            } onClick={() => handleCarSelection(cardetail.title)}
-             
-             >
-               
-            <CarImg src={cardetail.image} />
+  return (
+    <Wrapper>
+      <Title>select your Event</Title>
+      <CarList>
+        {carDetail.map((carDetail) => (
+          <Car
+            key={carDetail.id}
+            onClick={() => handleCarSelection(carDetail.title)}
+          >
+            <CarImg src={carDetail.image} />
             <CarDetails>
-                <Service>
-                    {cardetail.title}
-                </Service>
-                <Price>
-                Rs 52.00
-            </Price>
+              <Service>{carDetail.title}</Service>
+              <Price>Rs 52.00</Price>
             </CarDetails>
+          </Car>
+        ))}
+      </CarList>
+    </Wrapper>
+  );
+};
 
-           
-        </Car>
-            
-            )
-            }
-            
-                {/* {cardetail.map(car, index => (
-
-                    <Car key={ index }>
-                        
-                        
-                        <CarImg src= { cardetail.image} />
-                        <CarDetails>
-                            <Service>{cardetail.title}</Service>
-                            <Price> $52.00</Price>
-                        </CarDetails>
-                        <Time>8:03PM drop off</Time>
-                    </Car>
-                      ))
-        } */}
-                    
-            
-                    {/* </Link> */}
-            </CarList>
-        </Wrapper>
-    )
-}
-
-export default Rider
+export default Rider;
 
 const Wrapper = tw.div`
-flex-1 overflow-y-scroll flex flex-col
-`
+  flex-1 overflow-y-scroll flex flex-col
+`;
 const Title = tw.h1`
-text-gray-500 text-center text-xs py-2 border-b
-`
+  text-gray-500 text-center text-xs py-2 border-b
+`;
 const CarList = tw.div`
-overflow-y-scroll
-`
+  overflow-y-scroll
+`;
 const Car = tw.button`
-flex p-4 items-center justify-between border-b cursor-pointer hover:bg-gray-200 focus:outline-none focus:bg-gray-200 w-full text-left
-`
+  flex p-4 items-center justify-between border-b cursor-pointer hover:bg-gray-200 focus:outline-none focus:bg-gray-200 w-full text-left
+`;
 const CarImg = tw.img`
-h-10 mr-4
-`
+  h-10 mr-4
+`;
 const CarDetails = tw.div`
-flex-1 
-`
+  flex-1 
+`;
 const Price = tw.div`
-text-sm
-`
+  text-sm
+`;
 const Service = tw.div`
-font-medium
-`
+  font-medium
+`;
--- src/pages/confirm.js
+++ src/pages/confirm.js
@@ -1,117 +1,73 @@
-import React from 'react'
-import tw from 'tailwind-styled-components'
-import { useEffect,useState } from 'react'
-import Map from'../Components/Map'
-import { useRouter } from 'next/router'
-import Rider from './Rider'
-import Link from 'next/link'
+import React from 'react';
+import tw from 'tailwind-styled-components';
+import { useEffect, useState } from 'react';
+import MapComponent from '../Components/Map';
+import { useRouter } from 'next/router';
+import Rider from './Rider';
+import Link from 'next/link';
+
 const Confirm = () => {
-    const router = useRouter()
+  const router = useRouter();
 
-    const {pick} = router.query
-    const {sdate} = router.query
-    const {edate} = router.query
-  
+  const { pick } = router.query;
+  const { sdate } = router.query;
+  const { edate } = router.query;
 
+  const [pickup, setPickup] = useState();
 
-
-    const [pickup, setPickup] = useState()
-
-    
-     
-    const getPickupCoordinates = (pick) => {
-        const pickup = pick;
-        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?`+
+  const getPickupCoordinates = (pick) => {
+    const pickup = pick;
+    fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?` +
         new URLSearchParams({
-            access_token: "pk.eyJ1IjoicGFydmVlbjIzIiwiYSI6ImNsZ254a29rMDAwaGgzaW1yaWJwMWJ6Mm4ifQ.3VxAjPvVhzv24X-pHvCG_g"
-            // limit: 1
+          access_token:
+            'pk.eyJ1IjoicGFydmVlbjIzIiwiYSI6ImNsZ254a29rMDAwaGgzaW1yaWJwMWJ6Mm4ifQ.3VxAjPvVhzv24X-pHvCG_g',
         })
-        )
-        .then(response => response.json())
-        .then(data => {
-            // console.log(data.features[0].center)
-
-            const coordinates = data.features[0].center
-            setPickup(coordinates)
-            
-        })
-
-
-        }
-        // const getDropoffCoordinates = (drop) => {
-        //     const dropoff = drop;
-        //     fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropoff}.json?`+
-        //     new URLSearchParams({
-        //         access_token: "pk.eyJ1IjoicGFydmVlbjIzIiwiYSI6ImNsZ254a29rMDAwaGgzaW1yaWJwMWJ6Mm4ifQ.3VxAjPvVhzv24X-pHvCG_g"
-        //         // limit: 1
-        //     })
-        //     )
-        //     .then(response => response.json())
-        //     .then(data => {
-        //         // console.log(data.features[0].center)
-    
-        //         const coordinates = data.features[0].center
-        //         setDropoff(coordinates)
-        //     })
-    
-        // }
-    useEffect(() => {
-        getPickupCoordinates(pick);
-        // getDropoffCoordinates(drop);
-    }, [pick])
-
-    return(
-        <Wrapper>
-           <ButtonContainer>
-            <Link href="/">
-                <BacKButton src="https://img.icon8.com/ios-filled/50/000000/left.png"/>
-            </Link>
-            </ButtonContainer> 
-        <Map
-            pickup={pickup}
-           
-        />
-            <RideContainer>
-                <Rider
-                pickup={pick}
-                sdate={sdate}
-                edate={edate}
-
-                />
-
-               
-
-                
-                {/* <ConfirmButtonContainer>  
-                    <ConfirmButton>
-                        Confirm Ride
-                    </ConfirmButton>
-                </ConfirmButtonContainer> */}
-            </RideContainer>
-        </Wrapper>
     )
-}
+      .then((response) => response.json())
+      .then((data) => {
+        const coordinates = data.features[0].center;
+        setPickup(coordinates);
+      });
+  };
 
-export default Confirm
+  useEffect(() => {
+    getPickupCoordinates(pick);
+  }, [pick]);
+
+  return (
+    <Wrapper>
+      <ButtonContainer>
+        <Link href="/">
+          <BacKButton src="https://img.icon8.com/ios-filled/50/000000/left.png" />
+        </Link>
+      </ButtonContainer>
+      <MapComponent pickup={pickup} />
+      <RideContainer>
+        <Rider pickup={pick} sdate={sdate} edate={edate} />
+      </RideContainer>
+    </Wrapper>
+  );
+};
+
+export default Confirm;
 
 const RideContainer = tw.div`
-fex-1 flex  flex-col h-1/2
- `
+  fex-1 flex flex-col h-1/2
+`;
 const Wrapper = tw.div`
-flex   flex-col  h-screen
-`
+  flex flex-col h-screen
+`;
 
 const ConfirmButton = tw.div`
-bg-black text-white text-center my-4 mx-4 py-4 rounded-lg text-xl
-`
+  bg-black text-white text-center my-4 mx-4 py-4 rounded-lg text-xl
+`;
 const ConfirmButtonContainer = tw.div`
-border-t-2
-`
+  border-t-2
+`;
 const ButtonContainer = tw.div`
-roudned-full  absolute top-4 left-4 z-10 bg-white shadow-md cursor-pointer
-`
+  rounded-full absolute top-4 left-4 z-10 bg-white shadow-md cursor-pointer
+`;
 const BacKButton = tw.img`
-h-full object-contain
-`
-
-
+  h-full object-contain
+`;
