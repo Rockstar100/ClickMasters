c924c891db23fc43d6923854df7b6303a2c5c84b
Rockstar100 <boyrockstar100@gmail.com>
2023-06-02 23:19:07 +0530

    changes

--- src/pages/Drawers.js
+++ src/pages/Drawers.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import tw from 'tailwind-styled-components'
+import React from 'react';
+import tw from 'tailwind-styled-components';
 import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
@@ -20,28 +20,28 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import Button from '@mui/material/Button';
-import Link from 'next/link'
+import Link from 'next/link';
+
 const drawerWidth = 240;
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-   
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: `-${drawerWidth}px`,
-    ...(open && {
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginLeft: 0,
-    }),
+const Wrapper = tw.div`flex`;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
   }),
-);
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
 
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== 'open',
@@ -70,15 +70,32 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 }));
 
 export default function Drawers() {
-    const theme = useTheme();
-      const [open, setOpen] = React.useState(false);
-      const handleDrawerClose = () => {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const handleDrawerClose = () => {
     setOpen(false);
   };
+
   return (
-<Wrapper>
-     <Drawers/>
-    <Drawer
+    <Wrapper>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() => setOpen(true)}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            My App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
         sx={{
           width: drawerWidth,
           flexShrink: 0,
@@ -92,101 +109,49 @@ export default function Drawers() {
         open={open}
       >
         <DrawerHeader>
-                  <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-          Click Masters
-          </Typography>
-          <div style={{ marginLeft: 'auto' }}>
-                               <Button  sx={{ borderColor: 'white', color: 'white', mr: 1 }} variant="outlined">Sign in</Button>
-                             {/* <Button  sx={{ borderColor: 'white', color: 'white' }} variant="outlined">Sign Up</Button>
-                             */}
-                        </div>
-        </Toolbar>
-      </AppBar>
           <IconButton onClick={handleDrawerClose}>
             {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
           </IconButton>
         </DrawerHeader>
         <Divider />
         <List>
-          <Link href="/Login">
-            <ListItem  disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                 <InboxIcon /> 
-                </ListItemIcon>
-                <ListItemText primary="Home" />
-              </ListItemButton>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
             </ListItem>
-            </Link>
-
-            <Link href="/MyProfile">
-            <ListItem  disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                 <InboxIcon /> 
-                </ListItemIcon>
-                <ListItemText primary="My Profile" />
-              </ListItemButton>
-            </ListItem>
-            </Link>
-            <Link href="/SecurityPrivacy">
-            <ListItem  disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                 <InboxIcon /> 
-                </ListItemIcon>
-                <ListItemText primary="Security & Privacy" />
-              </ListItemButton>
-            </ListItem>
-            </Link>
-            <Link href="/TermsCondition">
-            <ListItem  disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                 <InboxIcon /> 
-                </ListItemIcon>
-                <ListItemText primary="Terms & Condition" />
-              </ListItemButton>
-            </ListItem>
-            </Link>
-            <Link href="/Help">
-            <ListItem  disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                 <InboxIcon /> 
-                </ListItemIcon>
-                <ListItemText primary="Help" />
-              </ListItemButton>
-            </ListItem>
-            </Link>
+          ))}
         </List>
         <Divider />
         <List>
           {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
             </ListItem>
           ))}
         </List>
       </Drawer>
-      </Wrapper>
-  )
+      <Main open={open}>
+        <DrawerHeader />
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+        </Typography>
+      </Main>
+    </Wrapper>
+  );
 }
 
-const Wrapper = tw.div``
