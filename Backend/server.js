237e2951464e07b5d17d946ec3ad5d5cda8f2265
Rockstar100 <boyrockstar100@gmail.com>
2023-06-05 15:22:16 +0530

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(cors({ origin: 'http://localhost:3000',
+app.use(cors({ origin: 'https://main--clickmaster.netlify.app/',
 credentials: true
  }));
 
